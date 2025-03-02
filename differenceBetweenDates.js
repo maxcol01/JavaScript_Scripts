9842e972c74eb98b06c2aeb03906e5a42441778f
maxcol01 <159482922+maxcol01@users.noreply.github.com>
2025-03-02 16:28:45 +0100

    new logic

--- differenceBetweenDates.js
+++ differenceBetweenDates.js
@@ -11,6 +11,11 @@ inputs = [
 ]
 
 
+const formatInput = function(inputs){
+    const date1 = inputs[1]+"/"+inputs[0]+"/"+inputs[2]
+    const date2 = inputs[4]+"/"+inputs[3]+"/"+inputs[5]
+    return {date1, date2}
+}
 
 const isLeap = function(year){
     // condition: divisable by for BUT not by a hundered at the same time OR divisable by 400 
@@ -20,18 +25,32 @@ const isLeap = function(year){
 
 
 const numberOfDaysBetweenDates = function(start, end){
-    let numberOfDays = Math.floor(end-start)/(3600*24*1000);
+    const startDate = new Date(start);
+    const endDate = new Date(end)
+    let numberOfDays = Math.floor((endDate-startDate)/(3600*24*1000));
     return numberOfDays;
     // return the number of days between two dates
 }
 
 const difference = function(date1, date2){
-    console.log(numberOfDaysBetweenDates(date1,date2))
+    const differenceInDays = numberOfDaysBetweenDates(date1, date2)
+    // check for the years:
+    let numberOfYear = Math.floor(differenceInDays/365);
+    let remainderInDays = differenceInDays - (numberOfYear*365)
+    // check for the number of LeapYears (number to substract to the remainder !)
+    let counterLeapYear = 0;
+    for (let i = new Date(date1).getFullYear(); i<= new Date(date2).getFullYear(); i++){
+        if (isLeap(i)){
+            counterLeapYear ++
+        }
+    }
+    return `${numberOfYear} years, ${remainderInDays-1} days`
 }
 
+const dates = formatInput(inputs)
 
-const startDate = "1/1/1939"; // formalism of the date is mm/dd/yyyy
-const endDate = "1/5/1940"; // formalism of the date is mm/dd/yyyy
+const startDate = dates.date1; // formalism of the date is mm/dd/yyyy
+const endDate = dates.date2; // formalism of the date is mm/dd/yyyy
 
 
 console.log(difference(startDate, endDate));
